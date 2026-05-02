9a4c1a47476e23e54e2546be538066cb8253b038
heidyvasquez2 <hcvasquez@ucundinamarca.edu.co>
2026-05-02 16:15:40 -0500

    fix: corregir AndroidManifest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,11 +6,8 @@ plugins {
 
 android {
     namespace = "com.example.demeter_pgc"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 36
+
 
     defaultConfig {
         applicationId = "com.example.demeter_pgc"
@@ -32,9 +29,10 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
+
     buildFeatures {
         compose = true
     }
